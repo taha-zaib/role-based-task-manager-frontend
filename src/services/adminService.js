40d658611d7cdebdf356672365f4e7d6e772c5c7
dev <dev@gmail.com>
2026-06-04 03:43:17 +0500

    fixed promote user bug

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -5,6 +5,7 @@ const API_URL = 'https://role-based-task-manager-production.up.railway.app/api/a
 export const getUsers = async (token) => {
     const response = await axios.get(
         `${API_URL}/users`,
+        {},
         {
             headers: {
                 Authorization: `Bearer ${token}`
@@ -18,6 +19,7 @@ export const getUsers = async (token) => {
 export const promoteUserToAdmin = async (id, token) => {
     const response = await axios.patch(
         `${API_URL}/users/${id}/promote`,
+        {},
         {
             headers: {
                 Authorization: `Bearer ${token}`
